235239bccdc06555fceac693c06d2b454d65c3bb
Kinrany <kinrany@yandex.com>
2018-03-17 22:13:10 +0300

    added new graph format

---
+++ docs/formats/graph.js
@@ -0,0 +1,73 @@
+/*
+  * {
+  *   nodes: [
+  *     {
+  *       id: 1,
+  *       label: '1'
+  *     },
+  *     {
+  *       id: 2,
+  *       label: '23'
+  *     }
+  *   ],
+  *   edges: [
+  *     {
+  *       id: '1-2',
+  *       source: 1,
+  *       target: 2
+  *     }
+  *   ],
+  *   last_node_index: 2
+  * }
+*/
+
+var GraphFormat = {
+    Node(id) {
+        return {
+            id: id,
+            label: id.toString()
+        };
+    },
+
+    Edge([s, t]) {
+        return {
+            data: {
+                id: `(${s}, ${t})`,
+                source: s,
+                target: t
+            }
+        };
+    },
+
+    Graph(nodes, edges) {
+        return {
+            last_node_index: nodes.max(),
+            nodes: nodes.map(Node),
+            edges: edges.map(Edge),
+        };
+    },
+
+    add_node() {
+        this.last_node_index += 1;
+        let node = Node(this.last_node_index);
+        this.nodes.push(node);
+    },
+
+    delete_nodes(node_id_list) {
+        this.nodes = this.nodes
+            .filter(node => !node_id_list.includes(node.data.id));
+
+        this.edges = this.edges
+            .filter(edge => !node_id_list.includes(edge.data.source))
+            .filter(edge => !node_id_list.includes(edge.data.target));
+    },
+
+    delete_edges(edge_id_list) {
+        this.edges = this.edges
+            .filter(edge => !edge_id_list.includes(edge.data.id));
+    },
+
+    get_elements() {
+        return this.nodes.concat(this.edges);
+    }
+};
