c230bc808235d92c676e60e89b4bea4e77a357bb
Kinrany <kinrany@yandex.com>
2018-04-29 17:22:02 +0300

    Cleanup graph.js and cygraph.js

--- src/formats/cygraph.js
+++ src/formats/cygraph.js
@@ -1,76 +1,35 @@
-/*
-  * {
-  *   nodes: [
-  *     {
-  *       data: {
-  *         id: '1',
-  *         label: '1'
-  *       },
-  *       position: {
-  *         x: 123,
-  *         y: 456
-  *       }
-  *     },
-  *     {
-  *       data: {
-  *         id: '2',
-  *         label: '23'
-  *       },
-  *       position: {
-  *         x: 789,
-  *         y: 102
-  *       }
-  *     }
-  *   ],
-  *   edges: [
-  *     {
-  *       data: {
-  *         id: '(1, 2)',
-  *         source: '1',
-  *         target: '2'
-  *       }
-  *     }
-  *   ]
-  * }
-*/
-
 var CyGraphFormat = {
 
-    // id: number || string
-    Node(id) {
-        return {
-            data: {
-                id: id.toString(),
-                label: id.toString()
-            },
-            position: { x: -1000, y: -1000 }
-        };
-    },
+  Node(id) {
+    return {
+      data: {
+        id: id.toString(),
+        label: id.toString()
+      },
+      position: { x: -1000, y: -1000 }
+    };
+  },
 
-    // s: number || string, t: number || string
-    Edge([s, t]) {
-        return {
-            data: {
-                id: `(${s}, ${t})`,
-                source: s.toString(),
-                target: t.toString()
-            }
-        };
-    },
+  Edge([s, t]) {
+    return {
+      data: {
+        id: `(${s}, ${t})`,
+        source: s.toString(),
+        target: t.toString()
+      }
+    };
+  },
 
-    // nodes: Array<number>, edges: Array<[number, number]>
-    Graph(nodes, edges) {
-        return {
-            nodes: nodes.map(CyGraphFormat.Node),
-            edges: edges.map(CyGraphFormat.Edge),
-        };
-    },
+  Graph(nodes, edges) {
+    return {
+      nodes: nodes.map(CyGraphFormat.Node),
+      edges: edges.map(CyGraphFormat.Edge),
+    };
+  },
 
-    // graph: GraphFormat.Graph
-    // returns: (CyGraphFormat.Node | CyGraphFormat.Edge)[]
-    elements_from_graph(graph) {
-        const nodes = graph.nodes.map(({ id }) => CyGraphFormat.Node(id));
-        const edges = graph.edges.map(({ source, target }) => CyGraphFormat.Edge([source, target]));
-        return nodes.concat(edges);
-    },
+  elements_from_graph(graph) {
+    const nodes = graph.nodes.map(({ id }) => CyGraphFormat.Node(id));
+    const edges = graph.edges.map(({ source, target }) => CyGraphFormat.Edge([source, target]));
+    return nodes.concat(edges);
+  },
 };
--- src/formats/graph.js
+++ src/formats/graph.js
@@ -1,84 +1,50 @@
-/*
-  * {
-  *   nodes: [
-  *     {
-  *       id: '1',
-  *       label: '1'
-  *     },
-  *     {
-  *       id: '2',
-  *       label: '23'
-  *     }
-  *   ],
-  *   edges: [
-  *     {
-  *       id: '1-2',
-  *       source: '1',
-  *       target: '2'
-  *     }
-  *   ],
-  *   last_node_index: 2
-  * }
-*/
-
 var GraphFormat = {
 
-    // id: number || string
-    Node(id) {
-        return {
-            id: id.toString(),
-            label: id.toString()
-        };
-    },
+  Node(id) {
+    return {
+      id: id.toString(),
+      label: id.toString()
+    };
+  },
 
-    // s: number || string, t: number || string
-    Edge([s, t]) {
-        return {
-            id: `(${s}, ${t})`,
-            source: s.toString(),
-            target: t.toString()
-        };
-    },
+  Edge([s, t]) {
+    return {
+      id: `(${s}, ${t})`,
+      source: s.toString(),
+      target: t.toString()
+    };
+  },
 
-    // nodes: Array<number>, edges: Array<[number, number]>
-    Graph(nodes, edges) {
-        return {
-            last_node_index: nodes.max(),
-            nodes: nodes.map(GraphFormat.Node),
-            edges: edges.map(GraphFormat.Edge),
-        };
-    },
+  Graph(nodes, edges) {
+    return {
+      last_node_index: nodes.max(),
+      nodes: nodes.map(GraphFormat.Node),
+      edges: edges.map(GraphFormat.Edge),
+    };
+  },
 
-    // this: GraphFormat.Graph
-    add_node() {
-        this.last_node_index += 1;
-        let node = GraphFormat.Node(this.last_node_index);
-        this.nodes.push(node);
-    },
+  add_node() {
+    this.last_node_index += 1;
+    let node = GraphFormat.Node(this.last_node_index);
+    this.nodes.push(node);
+  },
 
-    // this: GraphFormat.Graph, source: string, target: string
-    add_edge(source, target) {
-        let edge = GraphFormat.Edge([source, target]);
-        this.edges.push(edge);
-    },
+  add_edge(source, target) {
+    let edge = GraphFormat.Edge([source, target]);
+    this.edges.push(edge);
+  },
 
-    // this: GraphFormat.Graph, node_id_list: string[]
-    delete_nodes(node_id_list) {
-        this.nodes = this.nodes
-            .filter(node => !node_id_list.includes(node.id));
+  delete_nodes(node_id_list) {
+    this.nodes = this.nodes
+      .filter(node => !node_id_list.includes(node.id));
 
-        this.edges = this.edges
-            .filter(edge => !node_id_list.includes(edge.source))
-            .filter(edge => !node_id_list.includes(edge.target));
-    },
+    this.edges = this.edges
+      .filter(edge => !node_id_list.includes(edge.source))
+      .filter(edge => !node_id_list.includes(edge.target));
+  },
 
-    // this: GraphFormat.Graph, edge_id_list: string[]
-    delete_edges(edge_id_list) {
-        this.edges = this.edges
-            .filter(edge => !edge_id_list.includes(edge.id));
-    },
-
-    get_elements() {
-        return this.nodes.concat(this.edges);
-    }
+  delete_edges(edge_id_list) {
+    this.edges = this.edges
+      .filter(edge => !edge_id_list.includes(edge.id));
+  }
 };
