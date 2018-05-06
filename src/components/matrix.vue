7b136970e6ae17fca653fbfd96710735b1cf7135
Kinrany <kinrany@yandex.com>
2018-05-06 14:43:36 +0300

    Replace text editor and matrix editor with a switch that toggles between them.

--- src/components/matrix.vue
+++ src/components/matrix.vue
@@ -1,12 +1,26 @@
 <template>
   <div>
-    <matrix-editor></matrix-editor>
-    <text-editor style="padding-top: 6px"></text-editor>
+    <label>
+      <input type="checkbox" class="checkbox" v-model="as_text" />
+      {{label_text}}
+    </label>
+    <text-editor v-if="as_text"></text-editor>
+    <matrix-editor v-else></matrix-editor>
   </div>
 </template>
 
 <script lang="js">
 module.exports = {
+  data() {
+    return {
+      label_text: "как текст"
+    }
+  },
+  computed: {
+    label_text() {
+      return this.as_text ? "текст" : "таблица";
+    }
+  },
   components: {
     "matrix-editor": httpVueLoader("./matrix-editor.vue"),
     "text-editor": httpVueLoader("./text-editor.vue")
